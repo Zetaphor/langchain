92d9e5b1f5ff2849af2b7c2f9856deaac7f82428
Zetaphor <zetaphor@hey.com>
2023-03-10 20:10:44 -0600

    Push through output if no action found

--- langchain/agents/mrkl/base.py
+++ langchain/agents/mrkl/base.py
@@ -43,7 +43,9 @@ def get_action_and_input(llm_output: str) -> Tuple[str, str]:
     regex = r"Action: (.*?)[\n]*Action Input: (.*)"
     match = re.search(regex, llm_output, re.DOTALL)
     if not match:
-        raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+        # raise ValueError(f"Could not parse LLM output: `{llm_output}`")
+        print(f"No action found, providing raw output: ${llm_output}")
+        return "Final Answer", llm_output.strip()
     action = match.group(1).strip()
     action_input = match.group(2)
     return action, action_input.strip(" ").strip('"')
